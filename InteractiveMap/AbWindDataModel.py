4efa85ff4b3a8e63ece77c6e3c3f3d4c746e08b1
yfen3 <yfeng3@ualberta.ca>
2024-05-14 13:56:58 -0600

    0514: Visualization project: Some UI updates

---
+++ InteractiveMap/AbWindDataModel.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+os.environ["KERAS_BACKEND"] = "torch"
+import keras
+from InteractiveMap.DataLoader import DataLoader
+from InteractiveMap.ModelLoader import ModelLoader
+
+class AbWindDataModel:
+    def __init__(self):
+        self.df = pd.read_csv('Data/processed_ab_wind_test.txt')
+        self.nn_location = 'Visualization_proj_9stations.keras'
+
+    def get_latest_one_month_records_data_loader(self):
+        latest_one_month_records = self.df.sort_values('date', ascending=False).head(20*300)
+        data_loader = DataLoader()
+        data_loader.load_data_from_dataframe(latest_one_month_records)
+        
+        return data_loader
+
+
+    def get_prediction_model(self):
+        # Load the model
+        nn_model = keras.models.load_model(self.nn_location)
+        nn_model_loader = ModelLoader()
+        nn_model_loader.load_model(nn_model)
+
+        return nn_model_loader
+
--- InteractiveMap/Visualization.py
+++ InteractiveMap/Visualization.py
@@ -3,12 +3,10 @@ from InteractiveMap.Info import get_info_panel
 from InteractiveMap.Map import get_map
 from InteractiveMap.Tooltip import get_tooltip
 import InteractiveMap.Utils as Utils
-from InteractiveMap.DataLoader import DataLoader
-from InteractiveMap.ModelLoader import ModelLoader
-from sklearn.preprocessing import StandardScaler
-from dash import Dash, html, Input, Output, callback, exceptions
+from InteractiveMap.Graph import get_graph
+from InteractiveMap.AbWindDataModel import AbWindDataModel
+from dash import Dash, html, Input, Output, callback, exceptions, dcc
 import dash_leaflet as dl
-import pandas as pd
 
 # TODO Refactor this into a separate file
 import torch
@@ -25,37 +23,24 @@ print(f"Num GPUs Available: {torch.cuda.device_count()}")
 
 # Define the style of the app
 colors = {
-    'background': 'grey',
-    'text': 'yellow'
+    'background': '#19D3F3',
+    'text': '#AF0038'
 }
 
 # How many stations are used to predict
 number_of_stations = 9#11
 
 # Build the basic UI elements
-header = html.H4(children='Visualization of the FES project', style={'font_color': colors['text']})
-info = get_info_panel('Click on map to view the wind speed prediction', {'font_color': colors['text']})
+header = html.H4(children='Visualization of the FES project', style={'color': colors['text']})
+info = get_info_panel('Click on map to view the wind speed prediction', {'color': colors['text']})
 tooltip = get_tooltip([170, 180], 'Wind Speed: 10 km/h')
 map = get_map(children=[tooltip])
+graph = get_graph('Monthly Wind Speed Prediction')
+data_loader = AbWindDataModel().get_latest_one_month_records_data_loader()
+nn_model_loader = AbWindDataModel().get_prediction_model()
 
-# TODO Refactor this into a separate file
-df = pd.read_csv('Data/processed_ab_wind_test.txt')
-latest_one_month_records = df.sort_values('date', ascending=False).head(20*300)
-data_loader = DataLoader()
-data_loader.load_data_from_dataframe(latest_one_month_records)
-
-def get_prediction_model(data_loader):
-    # Load the model
-    nn_model = keras.models.load_model('Visualization_proj_9stations.keras')
-
-    nn_model_loader = ModelLoader()
-    nn_model_loader.load_model(nn_model)
-
-    return nn_model_loader
-
-nn_model_loader = get_prediction_model(data_loader)
-
-
+# TODO see if those callbacks can be moved to a separate file
+# Define callbacks
 @callback(
         Output(component_id="tooltip", component_property="position"), 
         Input(component_id="map", component_property="clickData"), 
@@ -100,14 +85,28 @@ def show_click_info(onClickData):
         lat, lng = Utils.get_latlng(onClickData)
         return f'Output: {lat}, {lng}'
 
+# UI elements
+graph_section = html.Div(
+    children=[map, graph], 
+    style={
+        'display': 'flex', 
+        'flexDirection':'row', 
+        'gap' : '10px',
+        'backgroundColor': colors['background']
+        }
+    )
 
 # Make the app and run it
 # the basic layout of the app is also defined here
 app = Dash(__name__)
-app.layout = html.Div(style={'backgroundColor': colors['background']}, 
-                      children=[
-                          header,
-                          info,
-                          map,
+app.layout = html.Div(
+    style={
+    'backgroundColor': colors['background'],
+    'padding': '10px'
+    }, 
+    children=[
+        header,
+        info,
+        graph_section
 ])
 app.run_server(debug=True)
